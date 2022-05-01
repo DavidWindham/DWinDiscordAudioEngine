40734937022c6c19a09e5dc13a6dc49d0ee0461c
DavidWindham <tinymouse2@gmail.com>
2022-05-01 14:02:34 +0100

    Removed old server classes that were re-written and replaced

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import inspect
 from functools import wraps
 
-from discord.errors import NotFound
 from discord.ext import commands
 from discord.utils import get
 
@@ -17,14 +16,13 @@ db.connect()
 servers = load_servers(db, client)
 
 
-# Decorator to remove a command message after running relevant command
-def remove_message_post_func_and_inject_server(func_to_run):
+# Decorator to inject server obj and remove a command message after running relevant command
+def inject_server_and_remove_message_after_func(func_to_run):
     @wraps(func_to_run)
     async def return_func(ctx, *args, **kwargs):
         server = get_server(ctx)
         await func_to_run(server, ctx, *args, **kwargs)
         await ctx.message.delete()
-        #await server.update_server_message(ctx.channel)
         await server.handle_server_message(ctx.channel)
 
     # Big thanks to Sierra Macleod for this solution
@@ -41,13 +39,13 @@ async def on_ready():
 
 
 @client.command(pass_context=True, help="Leave the voice channel the bot is currently in")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def leave(server, ctx):
     await server.leave_voice_channel(ctx.guild.voice_client)
 
 
 @client.command(pass_context=True, help="Use a URL for a youtube video to be added to the queue")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def play(server, ctx, url=None):
     voice_channel = ctx.message.author.voice.channel
     voice_clients = get(client.voice_clients, guild=ctx.guild)
@@ -55,49 +53,49 @@ async def play(server, ctx, url=None):
 
 
 @client.command(pass_context=True, help="Pause the current playback")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def pause(server, ctx):
     server.pause()
 
 
 @client.command(pass_context=True, help="Resume if the audio is paused")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def resume(server, ctx):
     server.resume()
 
 
 @client.command(pass_context=True, help="Stop playback, does not remove the current song from the queue")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def stop(server, ctx):
     server.stop()
 
 
 @client.command(pass_context=True, help="Skips the current track and plays the next in the queue")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def skip(server, ctx):
     server.skip()
 
 
 @client.command(pass_context=True, help="Embeds the playback queue in a message")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def queue(server, ctx):
     await server.handle_server_message(channel=ctx.channel)
 
 
 @client.command(pass_context=True, help="Embeds the playback queue in a message")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def history(server, ctx, history_length):
     server.get_history(limit=history_length)
 
 
 @client.command(pass_context=True, help="Clears all items in the playback queue")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def clear(server, ctx):
     server.clear_queue()
 
 
 @client.command(pass_context=True, help="Remove an item at a specific index, see the index using 'queue'")
-@remove_message_post_func_and_inject_server
+@inject_server_and_remove_message_after_func
 async def remove(server, ctx, index):
     server.remove_from_queue_at_index(int(index))
 
@@ -120,21 +118,6 @@ def get_server(ctx):
         )
         db.add_server(ctx.guild.id)
     return servers[ctx.guild.id]
-
-
-# async def send_queue_message(server, ctx):
-#     old_queue_message_id = server.get_queue_message_id()
-#     if old_queue_message_id is not None:
-#         try:
-#             old_queue_message = await ctx.channel.fetch_message(old_queue_message_id)
-#             await old_queue_message.delete()
-#         except NotFound:
-#             print("Message not found, likely deleted by another bot or client")
-#
-#     embedded_message = server.get_embedded_queue_message()
-#     queue_message = await ctx.channel.send(embed=embedded_message)
-#     server.set_queue_message_id(queue_message.id)
-#     server.set_server_message(queue_message)
 
 
 client.run(config.DISCORD_TOKEN)
