142494d79469262c43584389534dc5a3fa2499ab
DavidWindham <tinymouse2@gmail.com>
2022-05-02 01:08:38 +0100

    Fixed get_db function

--- database/db_interface.py
+++ database/db_interface.py
@@ -3,7 +3,6 @@ from database.stateless.stateless_DB import StatelessDB
 
 def get_db(server_uri: str, server_port: str):
     if server_uri is not None or server_uri == "" or server_port is not None or server_port == "":
-        #return MongoDB(server_uri, int(server_port))
-        return StatelessDB()
+        return MongoDB(server_uri, int(server_port))
 
-    return None
+    return StatelessDB()
